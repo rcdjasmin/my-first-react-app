03be08408803ee5bead834d3383f653e507e3e83
rcdjasmin <rcdjasmin@yahoo.com>
2019-09-10 22:39:13 +0800

    Updates

---
+++ precache-manifest.a1bba316988f4063ab5268ad64dd8905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad364126b36cdbbf380570ce77a400b2",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "6394aaff214f3df2826d",
+    "url": "/my-first-react-app/static/css/main.0f0e9144.chunk.css"
+  },
+  {
+    "revision": "134743f05aec1dc9b43b",
+    "url": "/my-first-react-app/static/js/2.20456f9e.chunk.js"
+  },
+  {
+    "revision": "6394aaff214f3df2826d",
+    "url": "/my-first-react-app/static/js/main.4244bf4f.chunk.js"
+  },
+  {
+    "revision": "aeebe059250f4282055f",
+    "url": "/my-first-react-app/static/js/runtime~main.5bb50234.js"
+  }
+]);
